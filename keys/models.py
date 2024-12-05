084009366ae2deea2eeddbf6594964a79a56a13a
Юрий-Александър Методиев <62445997+yuri1602@users.noreply.github.com>
2024-12-05 14:27:27 +0200

    Update models.py

--- keys/models.py
+++ keys/models.py
@@ -1,22 +1,22 @@
-from django.db import models
-
-# Create your models here
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import User  # Вграден модел за потребители
 
+# Персонализиран модел за потребители
 class CustomUser(AbstractUser):
     nfc_id = models.CharField(max_length=20, unique=True, null=True, blank=True)
 
     def __str__(self):
         return self.username
+
+
 class Key(models.Model):
     name = models.CharField(max_length=100)  # Име на ключа
     barcode = models.CharField(max_length=50, unique=True)  # Уникален баркод
     location = models.CharField(max_length=100)  # Локация на ключа
     is_issued = models.BooleanField(default=False)  # Дали е издаден
     issued_to = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, blank=True, related_name="issued_keys"
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name="issued_keys"
     )  # Кой потребител го е взел
     issued_at = models.DateTimeField(null=True, blank=True)  # Кога е издаден
 
@@ -29,7 +29,7 @@ class KeyHistory(models.Model):
         Key, on_delete=models.CASCADE, related_name="history"
     )  # Връзка към ключа
     user = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, blank=True
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
     )  # Потребител, който е взел/върнал ключа
     issued_at = models.DateTimeField()  # Време на издаване
     returned_at = models.DateTimeField(null=True, blank=True)  # Време на връщане
