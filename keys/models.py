cb72ade5675a5904bb983a305f82579f9c7120d6
Юрий-Александър Методиев <62445997+yuri1602@users.noreply.github.com>
2024-12-05 15:36:46 +0200

    test

--- keys/models.py
+++ keys/models.py
@@ -1,13 +1,9 @@
+from django.db import models
+
 # Create your models here
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User  # Вграден модел за потребители
 
-class CustomUser(AbstractUser):
-    nfc_id = models.CharField(max_length=20, unique=True, null=True, blank=True)
-
-    def __str__(self):
-        return self.username
 class Key(models.Model):
     name = models.CharField(max_length=100)  # Име на ключа
     barcode = models.CharField(max_length=50, unique=True)  # Уникален баркод
